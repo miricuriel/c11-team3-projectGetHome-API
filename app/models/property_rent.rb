018a06094fb5fd883d6fe5e254d7a2a1c4503c9b
Diego Alejandro Vera Trujillo <diegoverat1796@gmail.com>
2023-10-10 16:58:35 -0500

    Fixed: asociaciones

--- app/models/property_rent.rb
+++ app/models/property_rent.rb
@@ -1,3 +1,5 @@
 class PropertyRent < ApplicationRecord
   belongs_to :property
+  has_many :user_property_rents
+  has_many :users, through: :user_property_rents
 end
--- app/models/user_property_sale.rb
+++ app/models/user_property_sale.rb
@@ -1,4 +1,5 @@
 class UserPropertySale < ApplicationRecord
   belongs_to :user
-  has_and_belong_to_many :property_sales
+  # has_and_belong_to_many :property_sales
+  belongs_to :property_sale
 end
