7ecbdb379103fcd7a798ae3e14a10c7a0af0b559
Diego Alejandro Vera Trujillo <diegoverat1796@gmail.com>
2023-10-16 22:27:51 -0500

    test

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,6 @@ Rails.application.routes.draw do
   resources :user_property_sales, only: %i[index create]
   resources :user_property_rents, only: %i[index create]
 
-  resources :users, only: :create do
-    resources :property_users, only: [:show, :create, :index]
-  end
-  
   post "/signup", to: "users#create"
 
   get "/profile", to: "users#show"
@@ -19,11 +15,8 @@ Rails.application.routes.draw do
   
   resources :users, only: [:show]  do
     resources :properties, only: %i[update destroy show index]
-
     resources :property_users
-
     resources :user_property_sales
-
   end
  
   post "/login", to: "sessions#create"
